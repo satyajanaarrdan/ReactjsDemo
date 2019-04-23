6bfcc398962ad3e204c12232e67c1cac0e2c8e77
unknown <satya0450@gmail.com>
2019-04-23 10:31:54 +0530

    appjs file is modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { Fragment } from 'react';
+
 
 function App() {
+
+// let x=1;
+// let y=2;
+
   return (
-
-    <div>
-      <div className="header">
-        Phone Directory
-    </div>
-      <button>Add</button>
-      <div>
-        <span>Name</span><br />
-        <span>Phone</span>
-      </div>
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type Here" defaultValue="satya" />
-
-    </div>
-
+    React.createElement("div",{id:"header"}, React.createElement("p",null,"ReactJS"))
   );
 }
 
