7c4863112cdd1a8916fd154c22799eca1e48a2f7
unknown <satya0450@gmail.com>
2019-04-23 10:10:13 +0530

    appjs file is modified

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import React from 'react';
 function App() {
   return (
     <div>
-    <div>
-     Phone Directory
+      {/* <div className="header">
+        Phone Directory
     </div>
-    <button>Add</button>
-    <div>
-      <span>Name</span><br/>
-      <span>Phone</span>
-    </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div> */}
+        <label htmlFor="name">Name: </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="satya"/>
+
     </div>
   );
 }
