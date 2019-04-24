c2f51ddd6da2d6f03965fc255635d37194e12092
unknown <satya0450@gmail.com>
2019-04-24 09:18:04 +0530

    add header in addsubscriber.js file and render data out

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,14 +1,17 @@
 
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import Header from './Header'
 
+class AddSubscriber extends Component {
+    render() {
+        return (
 
-class AddSubscriber extends Component{
-    render()
-    {
-        return(
-
-                <div></div>
-
+            <div>
+                <Header heading="ADD Subscriber" />
+                <div className="component-body-container">
+                    <button className="custom-btn">Back</button>
+                </div>
+            </div>
         );
     }
 }
