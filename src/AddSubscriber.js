f49d76bf209b9318417139e0c187eaf46c9c4f26
unknown <satya0450@gmail.com>
2019-04-24 09:14:43 +0530

    new file AddSubscirber is added and done some change in index.js file instead of APP using AddSubscriber added in ReactDOM.render()

---
+++ src/AddSubscriber.js
@@ -0,0 +1,15 @@
+
+import React,{Component} from 'react';
+
+
+class AddSubscriber extends Component{
+    render()
+    {
+        return(
+
+                <div></div>
+
+        );
+    }
+}
+export default AddSubscriber;
