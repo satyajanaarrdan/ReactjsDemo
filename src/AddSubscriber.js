ffa3bf2ed750da1234cc20e9e77a60e4484a343b
unknown <satya0450@gmail.com>
2019-04-24 10:45:13 +0530

    change  event handler is added

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -4,6 +4,25 @@ import Header from './Header';
 import './AddSubscriber.css';
 
 class AddSubscriber extends Component {
+
+constructor(){
+    super();
+    this.state={
+        id:0,
+        name:'',
+        phone:''
+    }
+}
+
+inputChangedHanlder=(e)=>{
+    const state=this.state;
+    state[e.target.name]=e.target.value;
+    this.setState(state);
+    console.log(this.state);
+}
+
+
+
     render() {
         return (
 
@@ -14,11 +33,12 @@ class AddSubscriber extends Component {
                     <form className="subscirber-form">
                         <label htmlFor="name" className="label-control">Name: </label>
                         <br />
-                        <input type="text" id="name" className="input-control" name="name" /> <br /><br />
+                        <input type="text" id="name" className="input-control" name="name" onChange={this.inputChangedHanlder} /> <br /><br />
 
                         <label htmlFor="phone" className="label-control">Phone: </label>
                         <br />
-                        <input type="text" id="phone" className="input-control" name="phone" /> <br /><br />
+                        <input type="text" id="phone" className="input-control" name="phone" onChange={this.inputChangedHanlder} /> <br /><br />
+
                         <div className="subscriber-info-container">
                             <span className="subscriber-to-add-heading">Subscriber to be added</span><br />
                             <span className="subscriber-info">Name: </span><br />
