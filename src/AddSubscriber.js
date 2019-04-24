7eb5588a55455156a102427d3c40aba2777084e9
unknown <satya0450@gmail.com>
2019-04-24 09:43:09 +0530

    newly added data in common.css and addsubscriber.js file

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -12,6 +12,20 @@ class AddSubscriber extends Component {
                 <div className="component-body-container">
                     <button className="custom-btn">Back</button>
                     <form className="subscirber-form">
+                        <label htmlFor="name" className="label-control">Name: </label>
+                        <br />
+                        <input type="text" id="name" className="input-control" name="name" /> <br /><br />
+
+                        <label htmlFor="phone" className="label-control">Phone: </label>
+                        <br />
+                        <input type="text" id="phone" className="input-control" name="phone" /> <br /><br />
+                        <div className="subscriber-info-container">
+                            <span className="subscriber-to-add-heading">Subscriber to be added</span><br />
+                            <span className="subscriber-info">Name: </span><br />
+                            <span className="subscriber-info">Phone: </span>
+                        </div>
+                        <button type="submit" className="custom-btn add-btn">Add</button>
+
                     </form>
                 </div>
             </div>
